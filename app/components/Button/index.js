8c76601ec857455080878193485fb43e55718f34
Marian <marianmrva123@gmail.com>
2019-10-24 14:32:02 +0200

    Update button

--- app/components/Button/index.js
+++ app/components/Button/index.js
@@ -6,19 +6,26 @@
 
 import React, { Children } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
 // colorType options: primary, secondary, success, danger, warning, info, light, dark, link
-function Button(props) {
-  const right = props.right ? 'float-right' : '';
-  const type = props.colorType ? props.colorType : 'primary';
-  return (
+function Button({ colorType, children, right, onClick, href }) {
+  const positionRight = right ? 'float-right' : '';
+  const type = colorType || 'primary';
+  const button = (
     <button
-      onClick={props.onClick}
-      className={`btn btn-${type} ${right}`}
+      onClick={onClick}
+      className={`btn btn-${type} ${positionRight}`}
       type="button"
     >
-      {Children.toArray(props.children)}
+      {Children.toArray(children)}
     </button>
   );
+
+  if (href) {
+    return <Link to={href}>{button}</Link>;
+  }
+  return button;
 }
 
 Button.propTypes = {
@@ -26,6 +33,7 @@ Button.propTypes = {
   children: PropTypes.node.isRequired,
   right: PropTypes.bool,
   onClick: PropTypes.func,
+  href: PropTypes.string,
 };
 
 export default Button;
