f6fe384049a2fede6d33371b4dae1951cc9bd1fb
Marian <marianmrva123@gmail.com>
2019-10-25 18:49:29 +0200

    Add annotations to LineChart

--- app/components/LineChart/index.js
+++ app/components/LineChart/index.js
@@ -9,6 +9,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import { Line } from 'react-chartjs-2';
+import 'chartjs-plugin-annotation';
 import { createStructuredSelector } from 'reselect';
 import { makeSelectPatientByPatientId } from '../../containers/App/selectors';
 moment.suppressDeprecationWarnings = true;
@@ -77,6 +78,64 @@ function LineChart({ patientRecord }) {
                 displayFormats: {
                   hour: 'HH:mm',
                 },
+              },
+            },
+          ],
+          yAxes: [
+            {
+              display: true,
+              ticks: {
+                beginAtZero: true,
+                max: 160,
+              },
+            },
+          ],
+        },
+        annotation: {
+          annotations: [
+            {
+              type: 'line',
+              mode: 'horizontal',
+              scaleID: 'y-axis-0',
+              value: 60,
+              borderColor: 'blue',
+              borderWidth: 1,
+              label: {
+                enabled: true,
+                position: 'right',
+                content: 'Low',
+                backgroundColor: 'blue',
+              },
+            },
+            {
+              type: 'box',
+              // ID of the Y scale to bind onto
+              yScaleID: 'y-axis-0',
+              // Top edge of the box in units along the y axis
+              yMax: 105,
+              // Bottom edge of the box
+              yMin: 80,
+              backgroundColor: 'rgba(112,251,20, 0.2)',
+              borderWidth: 0.01,
+              label: {
+                enabled: true,
+                position: 'right',
+                backgroundColor: 'red',
+                content: 'High',
+              },
+            },
+            {
+              type: 'line',
+              mode: 'horizontal',
+              scaleID: 'y-axis-0',
+              value: 150,
+              borderColor: 'red',
+              borderWidth: 2,
+              label: {
+                enabled: true,
+                position: 'right',
+                backgroundColor: 'red',
+                content: 'High',
               },
             },
           ],
